b8655ae0519b6de74ccc5fb931c770aad32ff4ec
Tuan SPK <tuanspk@tuans-mac.local>
2018-05-31 01:08:59 +0700

    Initial Commit

---
+++ RainCatModification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainCatModification
+//
+//  Created by Tuan SPK on 5/31/18.
+//  Copyright © 2018 Tuan SPK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
